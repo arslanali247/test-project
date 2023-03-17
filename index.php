7974f925a5f1f8f539ed1efede981d70948415df
Arslan Ali <arslanali247@gmail.com>
2023-03-17 17:36:02 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello, World from Arslan Ali! <br>";
+echo "Hello Mashhoud Ho gaya Run! <br>";
+echo '<img src="https://scontent.flhe5-1.fna.fbcdn.net/v/t1.18169-9/15977072_1254552291291084_7446720250654642013_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=cdbe9c&_nc_eui2=AeF4VX0O425YU7gOn3k6A6hQSUIokrlL215JQiiSuUvbXqYvrZxcobUn4VGaFFL30Kge0CoPAWFCuOH0Ey-Kf7sn&_nc_ohc=sefb7eUc-PoAX-g_mOK&_nc_ht=scontent.flhe5-1.fna&oh=00_AfBmD9AXp1daQV0NKb94K2sd2rylfyBvIpILdsaTLHb_Vw&oe=640C1159">';
+
+?>
